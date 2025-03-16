c163d7fbcae62ae7ce01a429eca76a746b59c2c8
David Hdz <davidhernandeze@gmail.com>
2025-03-16 14:47:14 -0600

    fix call

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ set -a
 source .env
 set +a
 
-if [ "ENABLE_CORS" = "true" ]; then
-  curl -X PUT http://"$COUCHDB_USER":"$COUCHDB_PASS"@localhost:5984/_node/_local/_config/httpd/enable_cors -d '"true"'
-  curl -X PUT http://admin:password@localhost:5984/_node/_local/_config/cors/origins -d '"$ALLOWED_ORIGINS"'
+if [ "$ENABLE_CORS" = "true" ]; then
+  curl -X PUT http://"$COUCHDB_USER":"$COUCHDB_PASS"@localhost:5984/_node/nonode@nohost/_config/httpd/enable_cors -d '"true"'
+  curl -X PUT http://"$COUCHDB_USER":"$COUCHDB_PASS"@localhost:5984/_node/nonode@nohost/_config/cors/origins -d '"$ALLOWED_ORIGINS"'
 fi
 
 echo "Running Fauxton at https://$DOMAIN/_api/_utils/"
